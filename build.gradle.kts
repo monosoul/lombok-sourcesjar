6b14823fb1e9948ed8008522dbd469b7d0fd10b3
monosoul <Kloz.Klaud@gmail.com>
2019-02-01 19:50:39 +0500

    add delombok task

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ java {
 }
 
 dependencies {
-    val lombokDependency = "org.projectlombok:lombok:1.18.4"
+    val lombokDependency = "org.projectlombok:lombok:1.18.2"
 
     annotationProcessor(lombokDependency)
     compileOnly(lombokDependency)
@@ -29,9 +29,33 @@ tasks {
             )
         }
     }
-}
 
-tasks.register<Jar>("sourcesJar") {
-    from(sourceSets.main.get().allJava)
-    archiveClassifier.set("sources")
+    register<Jar>("sourcesJar") {
+        from(sourceSets.main.get().allJava)
+        archiveClassifier.set("sources")
+    }
+
+    register("delombok") {
+        group = "delombok"
+
+        doLast({
+            ant.withGroovyBuilder {
+                "taskdef"(
+                        "name" to "delombok",
+                        "classname" to "lombok.delombok.ant.Tasks\$Delombok",
+                        "classpath" to project.configurations.compileOnly.asPath)
+                "mkdir"("dir" to "build/src-delomboked")
+                "delombok"(
+                        "verbose" to "true",
+                        "encoding" to "UTF-8",
+                        "to" to "build/src-delomboked",
+                        "from" to "src/main/java"
+                ) {
+                    "format"("value" to "generateDelombokComment:skip")
+                    "format"("value" to "generated:generate")
+                    "format"("value" to "javaLangAsFQN:skip")
+                }
+            }
+        })
+    }
 }
